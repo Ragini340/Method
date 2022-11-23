feb1ac56d7fce7a059cb62076d19a22470b506f7
Ragini340 <ragini31bxr@gmail.com>
2022-11-23 11:11:31 +0530

    Add project files.

---
+++ Bike.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    abstract class Bike
+    {
+        //Anyone can call this
+        public void StartEngine() { }
+
+        //Only derived classes can call this
+        protected void AddGas(int v) { }
+
+        //Derived classes can override the base class implementation
+        public virtual int Drive(int miles, int speed)
+        {
+            return 1;
+        }
+
+        //Derived classes must implement this
+        public abstract double GetToSpeed();
+    }
+}
+
---
+++ TestBike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class TestBike : Bike
+    {
+        
+        public override double GetToSpeed()
+        {
+            return 100;
+        }
+        static void Main()
+        {
+
+           TestBike obj = new TestBike();
+
+            obj.StartEngine();
+            obj.AddGas(15);
+            obj.Drive(5, 20);
+            double speed = obj.GetToSpeed();
+            Console.WriteLine("Top speed is {0}", speed);
+        }
+    }
+}
+
+   
